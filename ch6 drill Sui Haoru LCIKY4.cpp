97bfa0a643d3de12aa053cae1e6c308e573e4363
Sui Haoru <78863977+Sui-i@users.noreply.github.com>
2021-03-12 19:49:39 +0100

    Update ch6 drill Sui Haoru LCIKY4.cpp

--- ch6 drill Sui Haoru LCIKY4.cpp
+++ ch6 drill Sui Haoru LCIKY4.cpp
@@ -1,24 +1,9 @@
-// This is example code from Chapter 6.7 "Trying the second version" of
-// "Software - Principles and Practice using C++" by Bjarne Stroustrup
-
-/*
-    This file is known as calculator02buggy.cpp
-
-    I have inserted 5 errors that should cause this not to compile
-    I have inserted 3 logic errors that should cause the program to give wrong results
-
-    First try to find an remove the bugs without looking in the book.
-    If that gets tedious, compare the code to that in the book (or posted source code)
-
-    Happy hunting!
-
-*/
-
-#include "E:\C++\std_lib_facilities.h"
+#include "std_lib_facilities.h"
 
 //------------------------------------------------------------------------------
 
-class Token{
+class Token
+{
 public:
     char kind;        // what kind of token
     double value;     // for numbers: a value 
@@ -28,7 +13,8 @@ public:
 
 //------------------------------------------------------------------------------
 
-class Token_stream {
+class Token_stream 
+{
 public:
     Token_stream();   // make a Token_stream that reads from cin
     Token get();      // get a Token (get() is defined elsewhere)
